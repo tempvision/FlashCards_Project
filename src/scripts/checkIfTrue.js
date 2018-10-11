d1c6ae3f99da4261525a71f96b9bff7ef2ba0dc0
Nayden <soodker@gmail.com>
2018-10-11 12:55:30 +0300

    Added function that checks if the answer is true

---
+++ src/scripts/checkIfTrue.js
@@ -0,0 +1,10 @@
+const checkIfTrue = function (id) {
+    if (questionMap.get(id))
+        console.log('true');
+    else console.log('false');
+}
+
+const buttons = Array.from(document.getElementsByClassName("button"));
+buttons.forEach((button) => {
+    button.addEventListener("click", () => { checkIfTrue(button.id) })
+})
