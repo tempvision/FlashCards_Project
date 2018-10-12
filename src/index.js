cd79392a142008ad75e6cd9da6b06d981b3ce097
Nayden <soodker@gmail.com>
2018-10-12 15:09:26 +0300

    pushed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import * as $ from 'jquery';
-import * as questionsOperations from './scripts/questions.oparations.js';
-import * as questions from './scripts/database.js';
-import * as flip from './scripts/jquery.flip.min.js';
-import * as cardOperations from './scripts/card.operations.js';
+import { init } from './scripts/init.js';
 
-const newCardSet = questionsOperations.createCardSet('JSAdvanced', 2);
-$('#cardText').append(`<p>${newCardSet[0].question}<p>`);
-$('#cardDescription').append(`<p>${newCardSet[0].longDescription}<p>`);
-$('#card').flip({ axis: 'x', trigger: 'manual' });
 
-$('#flipBtn').bind('click', () => {
-  cardOperations.cardFlipping('card');
+const [catName, cardAmount] = ['jsBasics', 5];
+
+
+$('#start').bind('click', () => {
+  init(catName, cardAmount);
+  $('#start').hide();
 });
--- src/scripts/checkIfTrue.js
+++ src/scripts/checkIfTrue.js
@@ -1,10 +1,26 @@
-const checkIfTrue = function (id) {
-    if (questionMap.get(id))
-        console.log('true');
-    else console.log('false');
-}
 
-const buttons = Array.from(document.getElementsByClassName("button"));
-buttons.forEach((button) => {
-    button.addEventListener("click", () => { checkIfTrue(button.id) })
-})
+
+const checkIfTrue = function(id, answers) {
+    // check if none of the first three is true
+    if (id === 3) {
+        let areAllFalse = true;
+        answers.forEach((answer) => {
+            if (answer.rigth) {
+                areAllFalse =false;
+            }
+        });
+        if (areAllFalse) {
+            // if all are false, then the answer is true!
+            window.sessionStorage.correctAmount++;
+        }
+        return areAllFalse;
+    }
+    // check if the answer is true
+    if (answers[id].rigth) {
+        window.sessionStorage.correctAmount++;
+        return true;
+    }
+    return false;
+};
+
+export { checkIfTrue };
