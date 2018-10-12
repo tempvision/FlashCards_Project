4636c3b181ec4d0fc653bad08aed7579864c9df9
Nayden <soodker@gmail.com>
2018-10-12 17:15:13 +0300

    Added Skip Button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as $ from 'jquery';
 import { init } from './scripts/init.js';
-import
+
 
 const [catName, cardAmount] = ['jsBasics', 5];
 
@@ -10,12 +10,4 @@ $('#start').bind('click', () => {
   $('#start').hide();
 });
 
-const skipButton = function() {
-  currentSet.unshift(currentQuestion);
-  createNext();
-};
 
-$('#skipButton').append('<p> SKIP </p>').bind('click', () => {
-  console.log('skipped');
-  skipButton();
-});
--- src/scripts/init.js
+++ src/scripts/init.js
@@ -2,14 +2,31 @@ import createCardset from './createCardSet.js';
 import createCardAnswersPair from './createCardAnswersPair.js';
 import endSession from './endSession.js';
 let currentSet = [];
+let currentQuestion = 2;
+import * as $ from 'jquery';
+
+const skipButton = function() {
+    $('#skipButton').append('<p> SKIP </p>').bind('click', () => {
+        console.log('YES'); currentSet.unshift(currentQuestion);
+        if (currentSet.length > 1) {
+            createNext();
+            console.log('no');
+        } else {
+            $('#skipButton').hide();
+        }
+    });
+};
+
 
 const createNext = function() {
     // creates the elements from the next question
+
     if (currentSet.length === 0) {
         endSession();
         return;
     }
-    const currentQuestion = currentSet.pop();
+
+    currentQuestion = currentSet.pop();
     createCardAnswersPair(currentQuestion);
     window.sessionStorage.currentQuestion++;
 };
@@ -17,6 +34,7 @@ const createNext = function() {
 
 const init = function(catName, questionsAmount) {
     // create the current set
+
     currentSet = createCardset(catName, questionsAmount);
     // starts results counter in session storage
     window.sessionStorage.questionsAmount = questionsAmount;
@@ -24,6 +42,7 @@ const init = function(catName, questionsAmount) {
     window.sessionStorage.currentQuestion = 0;
     // calls the creator function
     createNext();
+    skipButton();
 };
 
-export { init, createNext };
+export { init, createNext, currentSet };
